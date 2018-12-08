331a9b08bade8c6acd40ce7cfcd977ce335c2148
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2018-12-08 20:00:40 +0700

    Weird animation happening at the 00 hand possition workaround using math added. Now the time is calculated by the browser and not refreshig and getting it from the date object

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log('The time is ' + hr + ' hours ' + min + ' minutes ' + sec +' seconds');
+
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPosition = sec*360/60;
+
+
 function runTheClock() {
-    var date = new Date();
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    console.log('The time is ' + hr + ' hours ' + min + ' minutes ' + sec +' seconds');
-    
-    let hrPosition = (hr*360/12)+(min*(360/60)/12);
-    let minPosition = (min*360/60)+(sec*(360/60)/60);
-    let secPosition = sec*360/60;
+
+    hrPosition = hrPosition + (30/3600);
+    minPosition = minPosition + (1/60)*6;
+    secPosition = secPosition + 360/60;
     
     HOURHAND.style.transform = "rotate( " + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate( " + minPosition + "deg)";
